17d44354371ceebca388b68e9ce2a44a1382a96b
Scott <scott@erealmsoft.com>
2018-04-08 15:24:18 +0800

    selectionSort: change name of variable, 'minIndex' to 'index'

--- selectionSort.js
+++ selectionSort.js
@@ -9,13 +9,13 @@ const randomArr = require('./utils/radomArr');
 
 const selectionSort = (arr, direction) => { // 0 从小到大， 1 从大到小
     for (let i = 0; i < arr.length; i++) {
-        let minIndex = i;
+        let index = i;
         for (let j = i + 1; j < arr.length; j++) {
-            if (direction ? arr[j] > arr[minIndex] : arr[j] < arr[minIndex]) {
-                minIndex = j;
+            if (direction ? arr[j] > arr[index] : arr[j] < arr[index]) {
+                index = j;
             }
         }
-        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+        [arr[i], arr[index]] = [arr[index], arr[i]];
     }
 };
 let arr = randomArr(0, 10, 6);
